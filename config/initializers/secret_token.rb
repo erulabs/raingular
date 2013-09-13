ce77eada4de6cec9f5ddf83d25b0a2c0aa6206c2
Seandon Mooy <admin@erulabs.com>
2013-09-13 01:52:10 -0500

    angular + rails = raingular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Overcast::Application.config.secret_key_base = '54389744aa7186e98e0d1b8735948d186e514b456d2dc7de375c1fb16e1f7409423b6e8a451a67d7dec9122ce823d87a997e694a21453addacf5ab6826412ca4'
